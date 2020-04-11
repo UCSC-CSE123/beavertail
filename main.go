caf6ddc5ee1b43919092475d393fa415ed2264c4
Natan Lao <natanlao@users.noreply.github.com>
2020-04-11 12:16:14 -0700

    Add sqlite boilerplate

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./busdata.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	createStmt := `
+		CREATE TABLE IF NOT EXISTS Passengers(id INTEGER NOT NULL PRIMARY KEY,
+										bus INTEGER NOT NULL,
+										count INTEGER NOT NULL,
+										confidence INTEGER,
+										time INTEGER);
+	`
+	_, err = db.Exec(createStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, createStmt)
+		return
+	}
+}
