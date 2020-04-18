f32c39789f43e39897257eed61684fb0f270c713
Jorge Henriquez <contact@jorgehenriquez.dev>
2020-04-18 02:39:28 +0000

    Change: Move db connection to models package

---
+++ server/models/db.go
@@ -0,0 +1,16 @@
+// Package models defines all data models for the beavertail server.
+package models
+
+import "database/sql"
+
+// NewDB instantiates a new connection to the sqllite3 server.
+func NewDB(dataSource string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dataSource)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
