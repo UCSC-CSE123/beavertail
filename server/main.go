eb812a9317aa9c510d91b3076c4becbab2650511
Jorge Henriquez <contact@jorgehenriquez.dev>
2020-04-18 03:24:52 +0000

    Change: log.Print return to log.Fatal

--- server/main.go
+++ server/main.go
@@ -12,24 +12,21 @@ func main() {
 	// Establish a connection to the database.
 	db, err := models.NewDB("./busdata.db")
 	if err != nil {
-		log.Printf("failed to connect to db: %s\n", err)
-		return
+		log.Fatalf("failed to connect to db: %s\n", err)
 	}
 
 	// Inject the DB dependency into the server
 	// and init. the databases associated with it.
 	srv := datagram.NewServer(db)
 	if err := srv.InitDatabase(); err != nil {
-		log.Printf("failed to init. db: %s\n", err)
-		return
+		log.Fatalf("failed to init. db: %s\n", err)
 	}
 
 	// Start the server.
 	const host = "localhost"
 	const port = "3000"
 	if err := srv.Serve(host, port); err != nil {
-		log.Printf("server failed to start: %s", err)
-		return
+		log.Fatalf("server failed to start: %s", err)
 	}
 
 }
