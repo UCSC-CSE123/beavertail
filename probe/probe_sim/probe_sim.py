3c0c0166d7945c4eb07dfee5348b9b7455bc50b5
Natan Lao <natanlao@users.noreply.github.com>
2020-05-23 13:44:48 -0700

    Probe rewrite test unborked (#47)
    * Add: Initial probe simulation
    
    * import threading library for later
    
    * Add: start func
    
    * Move simulaiton to subdir
    
    * Do background updating
    
    * Add: Requirements.txt
    
    * Check for -1 when doing func calls
    
    Co-authored-by: Jorge Henriquez <contact@jorgeherniquez.dev>
    Co-authored-by: Jorge Henriquez <joahenri@ucsc.edu>
    Co-authored-by: Jorge Henriquez <contact@jorgehenriquez.dev>

---
+++ probe/probe_sim/probe_sim.py
@@ -0,0 +1,88 @@
+import random
+import threading
+import schedule
+import time
+
+
+class Register_Tester:
+
+    """
+      Register_Tester is a class that has a set of packets.
+
+      Every `add_time` seconds a new Packet is added to the set.
+      Every `del_time` seconds a Packet is dropped from the set.
+
+      If `add_time` or `remove_time` are negative, then its associated
+      action will be ignored (e.g. if add_time == -1 , then no packets are added).
+
+      In addition to adding and removing packets, Register_Tester will update a packet's
+      RSSI every `update_time` seconds.
+
+      When either a packet is updated or added the callback function prn will be called.
+    """
+
+    class Packet:
+
+        def __init__(self, addr2, strength):
+            self.addr2 = hash(addr2)
+            self.strength = float(strength)
+
+    def __init__(
+        self,
+        prn,
+        add_time,
+        del_time,
+        update_time,
+        ):
+
+        self.devices = set()
+        self.thread = None
+        self.add_time = add_time
+        self.del_time = del_time
+        self.update_time = update_time
+        self.prn = prn
+
+    def insert_new_packet(self, Packet):
+        self.prn(Packet)
+        self.devices.add(Packet)
+
+    def insert_new(self, addr2, strength):
+        self.insert_new_packet(self.Packet(addr2, strength))
+
+    def add(self):
+        if self.add_time == -1:
+            return
+        self.insert_new(random.getrandbits(128), random.randint(-127,
+                        128))
+
+    def remove(self):
+        if self.del_time == -1:
+            return
+        if len(self.devices) > 0:
+            self.devices.remove(random.sample(self.devices, 1)[0])
+
+    def update(self):
+        if self.update_time == -1:
+            return
+        if len(self.devices) > 0:
+            p = random.sample(self.devices, 1)[0]
+            self.devices.remove(p)
+            p.strength = p.strength + ((-0.5 if random.random()
+                    < 0.5 else 0.5))
+            self.insert_new_packet(p)
+
+    def start_async_helper(self):
+        schedule.every(self.add_time).seconds.do(self.add)
+        schedule.every(self.del_time).seconds.do(self.remove)
+        schedule.every(self.update_time).seconds.do(self.update)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    def start_async(self):
+        """
+        Calling start_async on the test register starts a new thread that updates this object auto-magically.
+        """
+
+        t = threading.Thread(target=self.start_async_helper)
+        t.start()
